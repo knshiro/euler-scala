9884aeb6d70a40ebf1a093872b370302b8d61a0a
Ugo Bataillard <ugo_bataillard@en-japan.com>
2015-08-06 09:40:47 +0900

    `::` works much better than `+:` !!!

--- src/main/scala/com/github/knshiro/s99/Problem2.scala
+++ src/main/scala/com/github/knshiro/s99/Problem2.scala
@@ -15,7 +15,7 @@ class Problem2 {
 
   @tailrec
   final def penultimateRec[T](l:Seq[T]):T = l match {
-    case x1 +: x2 +: Nil => x1
+    case x1 +: _ +: Nil => x1
     case x +: xs => penultimateRec(xs)
     case _ => throw new NoSuchElementException
   }
@@ -40,27 +40,45 @@ class Problem2 {
   }
 
   // Indeed it did close the gap with the imperative version!
+
+  // Let's try with :: (supposedly List specific) instead of +:
+  @tailrec
+  final def penultimateRec4[T](l:Seq[T]):T = l match {
+    case x1 :: _ :: Nil => x1
+    case _ :: xs => penultimateRec4(xs)
+    case _ => throw new NoSuchElementException
+  }
+
+  // IT HAS SAME PERFORMANCE AS IMPERATIVE VERSION!!
 }
 
 object Problem2 extends Problem2 with Problem {
   
   def run() = {
-    val list = List.range(1,100000)
+    val list = Seq.range(1,100000)
     println(list.last)
     println(penultimateImp(list))
     println(penultimateRec(list))
     println(penultimateRec2(list))
     println(penultimateRec3(list))
+    println(penultimateRec4(list))
     println("warm up")
     timeAverage(penultimateImp(list),200, doPrint = false)
     timeAverage(penultimateRec(list),200, doPrint = false)
     timeAverage(penultimateRec2(list),200, doPrint = false)
     timeAverage(penultimateRec3(list),200, doPrint = false)
+    timeAverage(penultimateRec4(list),200, doPrint = false)
 
+    println("penultimateImp(list)")
     timeAverage(penultimateImp(list),1000)
+    println("penultimateRec(list)")
     timeAverage(penultimateRec(list),1000)
+    println("penultimateRec2(list)")
     timeAverage(penultimateRec2(list),1000)
+    println("penultimateRec3(list)")
     timeAverage(penultimateRec3(list),1000)
+    println("penultimateRec4(list)")
+    timeAverage(penultimateRec4(list),1000)
   }
 
 }
